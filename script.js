96fc462999392f95dd1eb924befe793c3df0981e
zubaer <wfwffa@gmail.com>
2023-06-22 18:11:39 +0500

    js h/w

---
+++ script.js
@@ -0,0 +1,25 @@
+var userName = prompt('введите свое имя')
+console.log(typeof userName);
+
+var age = +prompt('введите свой возраст')
+
+console.log(typeof age);
+alert(age)
+
+
+var pr1 = +prompt('6+3=?')
+console.log('Пример 1: 6 + 3 = ' + (6 + 3) + ' (Ваш ответ ' + pr1 + ' )');
+
+var pr2 = +prompt('14-3=?')
+console.log('Пример 2: 14 - 3 = ' + (14 - 3) + ' (Ваш ответ ' + pr2 + ' )');
+
+var pr3 = +prompt('4*7=?')
+console.log('Пример 3: 6 + 3 = ' + 4 * 7 + ' (Ваш ответ ' + pr3 + ' )');
+
+var pr4 = +prompt('36/6=?')
+console.log('Пример 4: 36 / 6 = ' + 36 / 6 + ' (Ваш ответ ' + pr4 + ' )');
+
+var pr5 = +prompt('14%5=?')
+console.log('Пример 5: 14 % 5 = ' + 14 % 5 + ' (Ваш ответ ' + pr5 + ' )');
+
+
